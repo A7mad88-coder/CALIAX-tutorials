8ce5703d1471f031019db1cddb9903cbd92c20b3
omkar joglekar <joglekar.omkar@gmail.com>
2021-05-05 16:24:08 +0200

    added server URL's

--- pycom-ota-updates/config.py
+++ pycom-ota-updates/config.py
@@ -13,15 +13,15 @@ LED_blink_WIFI = 0x0000ff #Pink
 LED_SLEEP = 0x0000ff #Blue
 
    
-# URL DEV server 
-register= ""
-checkRollout= ""
-checkRolloutSuccess= ""
-bc_GetFirmware= ""
-ipfs_Download= ""
-hmac_auth = ""
+# Server URL's 
+register= "https://app.vc.asvin.io/api/device/register"
+checkRollout= "https://app.vc.asvin.io/api/device/next/rollout"
+checkRolloutSuccess= "https://app.vc.asvin.io/api/device/success/rollout"
+bc_GetFirmware= "https://app.besu.asvin.io/firmware/get"
+ipfs_Download= "https://app.ipfs.asvin.io/firmware/download"
+auth = "https://app.auth.asvin.io/auth/login"
 
-    
+        
 # Asvin Credentials
 customer_key=""
 device_key = ""
